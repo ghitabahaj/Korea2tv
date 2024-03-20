c7572c93d02216eeba2750cc53a8eb4373f9ce13
Ghita <bahajghita3@gmail.com>
2024-03-20 10:42:08 +0000

    add unit tests for save Media

--- Korea2Tv-Backend/src/test/java/com/youcode/korea2tv/services/impls/MediaServiceImplTest.java
+++ Korea2Tv-Backend/src/test/java/com/youcode/korea2tv/services/impls/MediaServiceImplTest.java
@@ -1,17 +1,20 @@
 package com.youcode.korea2tv.services.impls;
 
+import com.youcode.korea2tv.exception.custom.NotFoundException;
 import com.youcode.korea2tv.models.entity.Media;
 import com.youcode.korea2tv.repositories.MediaRepository;
-import com.youcode.korea2tv.services.MediaService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.Mockito.*;
 
 
@@ -27,15 +30,35 @@ class MediaServiceImplTest {
 
     @Test
     public void testSaveMedia() {
-        Media media = new Media();
+        Media media = new Media(); // create a Media object
+        when(mediaRepository.findMediaByOriginalTitleAndReleaseDate(anyString(), any(LocalDate.class)))
+                .thenReturn(Optional.empty()); // Media not found
         when(mediaRepository.save(media)).thenReturn(media);
 
-        // When
         Media savedMedia = mediaService.saveMedia(media);
 
-        // Then
         assertEquals(media, savedMedia);
         verify(mediaRepository, times(1)).save(media);
+    }
+
+    @Test
+    public void testSaveMediaWhenMediaNotFound() {
+        Media media = new Media(); // create a Media object
+        when(mediaRepository.findMediaByOriginalTitleAndReleaseDate(anyString(), any(LocalDate.class)))
+                .thenReturn(Optional.empty()); // Media not found
+
+        // Test and assert that NotFoundException is thrown
+        assertThrows(NotFoundException.class, () -> mediaService.saveMedia(media));
+    }
+
+    @Test
+    public void testSaveMediaWhenMediaAlreadyExists() {
+        Media media = new Media(); // create a Media object
+        when(mediaRepository.findMediaByOriginalTitleAndReleaseDate(anyString(), any(LocalDate.class)))
+                .thenReturn(Optional.of(media)); // Media already exists
+
+        // Test and assert that IllegalArgumentException is thrown
+        assertThrows(IllegalArgumentException.class, () -> mediaService.saveMedia(media));
     }
 
 
